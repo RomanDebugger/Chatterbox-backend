38c4042c28793e6c2511652cb0e359f4e2d6eef8
Akshat Tiwari <itsakshatts@gmail.com>
2025-06-30 10:29:06 +0530

    chore: remove client to split frontend and backend repos

--- client/app/lib/constants.js
+++
@@ -1,3 +0,0 @@
-// src/lib/constants.js
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
-export const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000';
